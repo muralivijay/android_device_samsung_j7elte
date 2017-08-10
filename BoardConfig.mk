079f9c3a9752d1c1621c523b4f8bc67b85c7ce32
Victor Lourme <22915886+vlourme@users.noreply.github.com>
2017-08-10 09:52:09 +0200

    j7eltexx: BoardConfig: remove auto "KERNEL IS NOT SEANDROID ENFORCING"
    Tested working on RR_N branch
    Based on 8890 code

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,7 @@ TARGET_KERNEL_CONFIG := j7elte_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/j7elte
 
 # Extracted with libbootimg
+BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 #BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(DEVICE_PATH)/dt.img --board SYSMAGIC000KU
 #TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
